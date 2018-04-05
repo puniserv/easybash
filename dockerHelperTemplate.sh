3e62ca7b8f63bcf960425d5eb0effc4f8694e997
Adam Dmitruczuk <adam.dmitruczuk@gforces.pl>
2018-04-05 09:15:01 +0200

    fix docker helper

--- dockerHelperTemplate.sh
+++ dockerHelperTemplate.sh
@@ -6,8 +6,7 @@ cd ${ABSOLUTE_DOCKER_COMPOSE_DIR}
 echo $(pwd)
 
 case "$1" in
-  'start') docker-compose start $2 ;;
-  'up') docker-compose up $2 ;;
   'exec'|'e'|'E') docker-compose exec $2 bash ;;
-  *) echo 'Supported actions: \n exec \n start \n up' ;;
+  '') echo 'Supported actions: \n exec \n e (exec alias) \n other docker-compose actions:'; docker-compose ;;
+  *) docker-compose $1
 esac
